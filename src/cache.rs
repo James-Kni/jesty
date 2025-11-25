e61a4324d6e61268832b0de7dab29ae3081e79a6
James Knight <Jameskn@protonmail.com>
2025-11-25 14:33:06 +0000

    fix: cache

--- src/cache.rs
+++ src/cache.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, sync::OnceLock};
 
 #[derive(Serialize, Deserialize)]
 pub struct Cache {
@@ -8,27 +8,30 @@ pub struct Cache {
     pub paths: Vec<String>,
 }
 
-fn cache_file() -> PathBuf {
-    dirs::cache_dir()
-        .unwrap_or_else(|| PathBuf::from(".cache"))
-        .join("jesty/last_run.json")
+fn cache_file() -> &'static PathBuf {
+    static PATH: OnceLock<PathBuf> = OnceLock::new();
+    PATH.get_or_init(|| {
+        dirs::cache_dir()
+            .unwrap_or_else(|| PathBuf::from(".cache"))
+            .join("jesty/last_run.json")
+    })
 }
 
 pub fn load_cache() -> Option<Cache> {
-    let p = cache_file();
-    if !p.exists() {
+    let path = cache_file();
+    if !path.exists() {
         return None;
     }
-    let s = fs::read_to_string(&p).ok()?;
-    serde_json::from_str(&s).ok()
+    let string = fs::read(&path).ok()?;
+    serde_json::from_slice(&string).ok()
 }
 
 pub fn save_cache(args: Vec<String>, paths: Vec<String>) -> Result<()> {
-    let c = Cache { args, paths };
-    let p = cache_file();
-    if let Some(d) = p.parent() {
-        fs::create_dir_all(d)?;
+    let cache = Cache { args, paths };
+    let path = cache_file();
+    if let Some(dir) = path.parent() {
+        fs::create_dir_all(dir)?;
     }
-    fs::write(&p, serde_json::to_string(&c)?)?;
+    fs::write(&path, serde_json::to_vec(&cache)?)?;
     Ok(())
 }
